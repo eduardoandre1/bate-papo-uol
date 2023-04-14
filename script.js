f891880e1a9a52ba74acbc0fe088f65d0ec17a6c
Eduardo André <eduardoandre418@gmail.com>
2023-04-14 18:39:38 -0300

    tela de login feita

--- script.js
+++ script.js
@@ -1,1 +1,30 @@
 axios.defaults.headers.common['Authorization'] = 'CXsWjs5oIZPtLIyHNdgCzRAl';
+let user = ""
+let servidor = ''
+function alertarresposta(resposta){
+    const status1 = resposta.status
+    const user_name = document.getElementById('user_name')
+    if(status1 === 200){
+        const login = document.querySelector('.login')
+        login.classList.add('disableclass')
+        return true
+    }
+    else if(status1 ===400){
+        alert('não deu certo')
+        user_name.innerHTML = ''
+        return false 
+}
+}
+
+function entrar(){
+    const user_name1 = document.getElementById('user_name')
+    if(user_name1.value !== ""){
+        user = {name:user_name.value}
+        let user_request = axios.post('https://mock-api.driven.com.br/api/vm/uol/participants ',user)
+        let status = user_request.then(alertarresposta)
+        console.log(status)  
+    }else{
+        alert("por favor ,digite algum nome para se identificar")
+        user_name1.innerHTML = ''
+    }
+}
