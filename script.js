256cf9ae1e7311d533a6f0a4e92a14f3b50319ca
Eduardo André <eduardoandre418@gmail.com>
2023-04-15 19:57:17 -0300

    removi alerts para ver se o data tester funciona

--- script.js
+++ script.js
@@ -1,27 +1,31 @@
 axios.defaults.headers.common['Authorization'] = 'CXsWjs5oIZPtLIyHNdgCzRAl';
 let user = null
 //login e entrar no servidor
-function alertarresposta(resposta){
+const user_block = (resposta) =>{
+    alert('usuario já existe')
+    user = null;
+    const login = document.querySelector('.login')
+    login.classList.remove('disableclass')
+    const user_name = document.getElementById('user_name')
+    user_name.innerHTML = ''
+}
+function login(resposta){
     const status1 = resposta.status
     const user_name = document.getElementById('user_name')
-    if(status1 === 200){
-        const login = document.querySelector('.login')
-        login.classList.add('disableclass')
-    }
-    else{
-        console.log('outro usuario já esta usando este nome')   
-        user_name.innerHTML = '' 
-    }
+    const login = document.querySelector('.login')
+    login.classList.add('disableclass')
 }
+
 function entrar(){
     const user_name1 = document.getElementById('user_name')
     if(user_name1.value !== ""){
         user = {name:user_name.value}
         let user_request = axios.post('https://mock-api.driven.com.br/api/vm/uol/participants ',user)
-        let status = user_request.then(alertarresposta)
+        user_request.then(login)
+        user_request.catch(user_block)
     }else{
         alert("por favor ,digite algum nome para se identificar")
-        user_name1.innerHTML = ''
+        user_name1.innerHTML = '23'
     }
 }
 //manter online
@@ -29,7 +33,6 @@ const conferironline = (resposta) =>{
     console.log('online')
 }
 const logoff = (resposta) =>{
-    alert('você esta deslogado')
     user = null;
     const login = document.querySelector('.login')
     login.classList.remove('disableclass')
@@ -85,4 +88,3 @@ function carregar_chat(){
 }
 setInterval(carregar_chat,3000)
 carregar_chat()
-//criar_text('bom dia linda')
